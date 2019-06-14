832d985ce7600aab980e6fd25d5a2a24f28fdc41
Seanwong933 <siegrainwong@gmail.com>
2019-06-14 22:01:02 +0800

    part1 almost finised except sqlserver persistence

--- Part1/Core.API/Program.cs
+++ Part1/Core.API/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Core.API
 {
@@ -19,7 +12,7 @@ namespace Core.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://0.0.0.0:5000")
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000");
     }
 }
